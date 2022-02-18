1fd7f46c1138968f66b76053b6a659a598bed315
Oliver-Boys <57505226+Oliver-Boys@users.noreply.github.com>
2022-02-18 17:06:20 +0000

    Create Lab3.java

---
+++ src/main/java/machine_learning_labs/Lab3.java
@@ -0,0 +1,8 @@
+package machine_learning_labs;
+
+import statistics.distributions.Distribution;
+import weka.classifiers.trees.j48.InfoGainSplitCrit;
+
+public class Lab3 {
+    InfoGainSplitCrit
+}
