93ea5eb29c6add0c1f815e131d6fcf6eb69ab48d
Oliver-Boys <57505226+Oliver-Boys@users.noreply.github.com>
2022-02-23 09:44:52 +0000

    Update Lab3.java

--- src/main/java/machine_learning_labs/Lab3.java
+++ src/main/java/machine_learning_labs/Lab3.java
@@ -1,8 +1,20 @@
 package machine_learning_labs;
 
-import statistics.distributions.Distribution;
+import experiments.data.DatasetLoading;
+import weka.classifiers.trees.j48.Distribution;
 import weka.classifiers.trees.j48.InfoGainSplitCrit;
+import weka.core.Instances;
+
+import java.io.IOException;
 
 public class Lab3 {
-    InfoGainSplitCrit
+    public static void golfExample() throws Exception {
+        Instances instances = DatasetLoading.loadData("E:\\MachineLearningData\\golf");
+        Distribution d = new Distribution(instances);
+
+        System.out.println(d.dumpDistribution());
+    }
+    public static void main(String[] args) throws Exception {
+        golfExample();
+    }
 }
